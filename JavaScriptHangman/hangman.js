953860b4d1166ca3fccfbd424c42ef064469eac3
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-02-23 21:36:45 +0100

    finish, add img and wave sound

--- JavaScriptHangman/hangman.js
+++ JavaScriptHangman/hangman.js
@@ -1,9 +1,12 @@
 let guesses = "Simple but not easy";
 guesses = guesses.toUpperCase();
 let hideGuesses = "";
+let misses = 0;
+let yes = new Audio("yes.wav");
+let no = new Audio("no.wav");
 
 for (let i = 0; i < guesses.length; i++) {
-    if (guesses.charAt(i) == " ") hideGuesses = hideGuesses + " ";
+    if (guesses.charAt(i) === " ") hideGuesses = hideGuesses + " ";
     else hideGuesses = hideGuesses + "-";
 }
 
@@ -58,7 +61,7 @@ function start() {
     for (let i = 0; i <= 34; i++) {
         let element = "let" + i;
         divContent = divContent + '<div class="letter" onclick="check(' + i + ')" id="' + element + '">' + letters[i] + '</div>';
-        if ((i + 1) % 7 == 0) divContent = divContent + '<div style="clear: both"></div>';
+        if ((i + 1) % 7 === 0) divContent = divContent + '<div style="clear: both"></div>';
     }
     document.getElementById("alphabet").innerHTML = divContent;
 
@@ -71,10 +74,43 @@ String.prototype.setChar = function (place, char) {
 }
 
 function check(number) {
+
+    let hit = false;
+
     for (let i = 0; i < guesses.length; i++) {
-        if (guesses.charAt(i) == letters[number]) {
+        if (guesses.charAt(i) === letters[number]) {
             hideGuesses = hideGuesses.setChar(i, letters[number]);
+            hit = true;
         }
     }
-    writeGuesses();
+    if (hit) {
+        yes.play();
+        let element = "let" + number;
+        document.getElementById(element).style.background = "#003300";
+        document.getElementById(element).style.color = "#00C000";
+        document.getElementById(element).style.border = "3px solid #00C000";
+        document.getElementById(element).style.cursor = "default";
+
+        writeGuesses();
+    } else {
+        no.play();
+        let element = "let" + number;
+        document.getElementById(element).style.background = "#330000";
+        document.getElementById(element).style.color = "#C00000";
+        document.getElementById(element).style.border = "3px solid #C00000";
+        document.getElementById(element).style.cursor = "default";
+        document.getElementById(element).setAttribute("onclick", ";");
+
+        misses++;
+        let picture = "img/s" + misses + ".jpg";
+        document.getElementById("gallows").innerHTML = '<img src="' + picture + '" alt=""/>';
+    }
+    if (guesses === hideGuesses) {
+        document.getElementById("alphabet").innerHTML = 'Yes, You win! Correct answer: ' + guesses +
+            '<br/><br/><span class="reset" onclick="location.reload()"> ONE MORE TIME? </span>';
+    }
+    if (misses === 9) {
+        document.getElementById("alphabet").innerHTML = 'Oh no, You lost! Correct answer: ' + guesses +
+            '<br/><br/><span class="reset" onclick="location.reload()"> ONE MORE TIME? </span>';
+    }
 }
